0c1d1c18dbd06e97b3bcd0447fe0f7933cd28aee
mamor <mamor.dev@gmail.com>
2013-02-02 17:04:26 +0900

    added FuelPHP v1.5.1

---
+++ fuel/app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.5
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * If you want to override the default configuration, add the keys you
+ * want to change here, and assign new values to them.
+ */
+
+return array(
+
+);
