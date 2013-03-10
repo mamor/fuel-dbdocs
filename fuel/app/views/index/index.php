00e615decdc84387f783e6df0852ea914fd3853f
mamor <mamor.dev@gmail.com>
2013-03-10 14:18:45 +0900

    fixed design root page for web

--- fuel/app/views/index/index.php
+++ fuel/app/views/index/index.php
@@ -3,9 +3,10 @@
 <table class="table table-bordered table-striped">
 <thead>
 <tr>
-<th style="width:34%;">Directory</th>
-<th style="width:33%;">Generated</th>
-<th style="width:33%;">Delete</th>
+<th style="width:5%;">No</th>
+<th style="width:45%;">Directory</th>
+<th style="width:20%;">Generated</th>
+<th style="width:15%;">Action</th>
 
 </tr>
 </thead>
@@ -14,6 +15,7 @@
 
 <?php foreach ($data['dirs'] as $dir): ?>
 <tr>
+<td><?php echo isset($i) ? ++$i : $i = 1; ?></td>
 <td><a href="<?php echo Uri::create('dbdocs/'.$dir['name'].'dbdoc/index.html'); ?>" target="_blank"><?php echo $dir['name'].'dbdoc/'; ?></a></td>
 <td><?php echo $dir['time_ago']; ?></td>
 <td><a href="<?php echo Uri::create('index/delete/'.$dir['name']); ?>"><i class="icon-trash"></i></a></td>
